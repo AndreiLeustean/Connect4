10a1cbe435fe7a53a07c34b33bb4911ed7d54e86
AndreiLeustean <163894215+AndreiLeustean@users.noreply.github.com>
2024-10-03 13:08:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let turn = 0;
-let redMoves = [];
-let yellowMoves = [];
+const redMoves = [];
+const yellowMoves = [];
 
 function changeColorCircle(idCircle) {
     if (turn % 2 === 0) {
-        document.getElementById(idCircle).style.backgroundColor = "#FF0000";
+        document.getElementById(idCircle).style.backgroundColor = "red";
     } else {
-        document.getElementById(idCircle).style.backgroundColor = "#FFD700";
+        document.getElementById(idCircle).style.backgroundColor = "yellow";
     }
 }
 
@@ -48,31 +48,21 @@ function winner(array) {
 }
 
 function findFreeCircle(arrowNumber) {
-    let firstColumn = [35, 28, 21, 14, 7, 0];
-    let secondColumn = [36, 29, 22, 15, 8, 1];
-    let thirdColumn = [37, 30, 23, 16, 9, 2];
-    let fourthColumn = [38, 31, 24, 17, 10, 3];
-    let fifthColumn = [39, 32, 25, 18, 11, 4];
-    let sixthColumn = [40, 33, 26, 19, 12, 5];
-    let seventhColumn = [41, 34, 27, 20, 13, 6];
-
     let array;
-    if (arrowNumber === 1) {
-        array = firstColumn;
-    } else if (arrowNumber === 2) {
-        array = secondColumn;
-    } else if (arrowNumber === 3) {
-        array = thirdColumn;
-    } else if (arrowNumber === 4) {
-        array = fourthColumn;
-    } else if (arrowNumber === 5) {
-        array = fifthColumn;
-    } else if (arrowNumber === 6) {
-        array = sixthColumn;
-    } else if (arrowNumber === 7) {
-        array = seventhColumn;
+    let matrix = [
+        [35, 28, 21, 14, 7, 0],
+        [36, 29, 22, 15, 8, 1],
+        [37, 30, 23, 16, 9, 2],
+        [38, 31, 24, 17, 10, 3],
+        [39, 32, 25, 18, 11, 4],
+        [40, 33, 26, 19, 12, 5],
+        [41, 34, 27, 20, 13, 6]
+    ];
+    for (let i = 0; i < 7; ++i) {
+        if (arrowNumber === i + 1) {
+            array = matrix[i];
+        }
     }
-
     for (let i = 0; i < 6; ++i) {
         let idCircle = "box" + array[i];
         if (document.getElementById(idCircle).style.backgroundColor === "" ||
